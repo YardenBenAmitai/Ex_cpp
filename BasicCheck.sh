d7f65a00eac67e1e8ddf780267ea904d5906acb0
YardenBenAmitai <44947659+YardenBenAmitai@users.noreply.github.com>
2019-03-17 15:57:58 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -14,13 +14,13 @@ valgrind ./${ARRAY[@]} --lead-check=full --error-exitcode=33
 valgrindOUT=$?
 valgrind --tool=helgrind ./${ARRAY[@]} --error-exitcode=33
 helgrindOUT=$?
-if [ valgrindOUT -gt 0 && helgrindOUT -gt 0 ]
+if [[ valgrindOUT -gt 0 && helgrindOUT -gt 0 ]]
  then   
         exit 3
-elif [ valgrindOUT -gt 0 ]
+elif [[ valgrindOUT -gt 0 ]]
  then
         exit 2
-elif [ helgrindOUT -gt 0 ]
+elif [[ helgrindOUT -gt 0 ]]
  then 
         exit 1
 fi
