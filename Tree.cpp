e08a498d30a267c12b7cab231b4242b9924e3516
YardenBenAmitai <44947659+YardenBenAmitai@users.noreply.github.com>
2019-03-31 23:18:58 +0300

    ex3

--- Tree.cpp
+++ Tree.cpp
@@ -2,32 +2,30 @@
 #include "Tree.hpp"
 using namespace ariel;
 
-void Tree::destroy_tree(Node *leaf){
+void Tree::destroy_tree(struct Node *leaf){
 	if(leaf != NULL){
 		destroy_tree(leaf->getLeft());
 		destroy_tree(leaf->getRight());
 		delete leaf;
+		leaf=NULL;
 	}
 }
 
-void Tree::insert(Node* leaf, int key){
-	if(key < leaf->getValue()){
-		if(leaf->getLeft() != NULL){
-			insert(leaf->getLeft(), key);
-		}else{
-			leaf->setLeft(new Node(key));
-		}
+struct Node* Tree::insert(struct Node* leaf, int key){
+	if(leaf==NULL){
+		return new Node(key);
+	}else if(key < leaf->getValue()){
+		leaf->setLeft(insert(leaf->getLeft(), key));
 	}else if(key > leaf->getValue()){
-		if(leaf->getRight() != NULL){
-			insert(leaf->getRight(),key);
-		}else{
-			leaf->setRight(new Node(key));
-		}
-	}else
-		throw std::invalid_argument( "This value already exists!");
+		leaf->setRight(insert(leaf->getRight(), key));
+	}else{
+		leaf=NULL;
+		throw std::invalid_argument("insert: This value already exists!");
+	}
+	return leaf;
 }
 
-void Tree::inorder_print(Node *leaf){
+void Tree::inorder_print(struct Node *leaf){
 	if(leaf != NULL){
 		inorder_print(leaf->getLeft());
 		cout << leaf->getValue() << " ";
@@ -35,7 +33,7 @@ void Tree::inorder_print(Node *leaf){
 	}
 }
 
-Node* Tree::FindMax(Node* leaf){
+struct Node* Tree::FindMax(struct Node* leaf){
 	if(leaf==NULL)
 		return NULL;
 	while(leaf->getRight() != NULL){
@@ -44,52 +42,58 @@ Node* Tree::FindMax(Node* leaf){
 	return leaf;
 }
 
-void Tree::remove(Node* leaf,int key){
-	if(leaf==NULL)
-		throw std::invalid_argument( "No such value to remove");
+struct Node* Tree::remove(struct Node* leaf,int key){
+	if(leaf==NULL){
+		throw std::invalid_argument("remove: No such value to remove");
+		return NULL;
+	}
 	else if(key < leaf->getValue()) 
-		remove(leaf->getLeft(), key);
+		leaf->setLeft(remove(leaf->getLeft(), key));
 	else if (key > leaf->getValue())
-		remove(leaf->getRight(), key);
+		leaf->setRight(remove(leaf->getRight(), key));
 	else{
 		if(leaf->getRight() == NULL && leaf->getLeft() == NULL){
-			leaf = NULL;
+			delete leaf;
+			leaf=NULL;
 		}
-		else if(leaf->getRight() == NULL && leaf->getLeft()!=NULL){
-			Node* temp=leaf;
-			leaf=leaf->getLeft();
-			delete temp;
+		else if(leaf->getRight() == NULL){
+			struct Node* temp=leaf->getLeft();
+			delete leaf;
+			return temp;
 		}
+
 		else if(leaf->getLeft() == NULL){
-			Node* temp = leaf;
-			leaf= leaf->getRight();
-			delete temp;
+			struct Node* temp =leaf->getRight();
+			delete leaf;
+			return temp;
 		}
 		else{
-			Node* temp = FindMax(leaf->getLeft());
+			struct Node* temp = FindMax(leaf->getLeft());
 			leaf->setValue(temp->getValue());
-			remove(leaf->getLeft(), temp->getValue());
+			leaf->setLeft(remove(leaf->getLeft(), temp->getValue()));
 		}
 	}
+	return leaf;
 }
 
-int Tree::size(Node* leaf){
+int Tree::size(struct Node* leaf){
 	if (leaf == NULL)
 		return 0;
 	else
 		return(size(leaf->getLeft()) + 1 + size(leaf->getRight()));
 }
 
-Node* Tree::FindLeaf(Node *leaf, int key){
-	
+struct Node* Tree::FindLeaf(struct Node *leaf, int key){	
 	if ( leaf->getValue() > key && leaf->getLeft()!=NULL){
 		if(leaf->getLeft()->getValue()==key)
 			return leaf->getLeft();
-		FindLeaf(leaf->getLeft(), key);
+		else
+			return FindLeaf(leaf->getLeft(), key);
 	}else if (leaf->getValue() < key && leaf->getRight()!=NULL){
 		if(leaf->getRight()->getValue()==key)
 			return leaf->getRight();
-		FindLeaf(leaf->getRight(), key);
+		else
+			return FindLeaf(leaf->getRight(), key);
 	}
 	return NULL;
 }
