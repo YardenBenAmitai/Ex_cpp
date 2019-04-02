3496ae3429ae08413c4438ab0cb0cbdc5459db98
YardenBenAmitai <44947659+YardenBenAmitai@users.noreply.github.com>
2019-04-02 14:50:36 +0300

    Update Tree.hpp

--- Tree.hpp
+++ Tree.hpp
@@ -8,8 +8,8 @@ struct Node{
 		struct Node *left;
 		struct Node *right;
 	public:
-		Node(int val){cout<<"construct key"<<endl; value=val; right=NULL; left=NULL;}
-		Node(struct Node* l, struct Node* r, int val) { cout<<"construct full"<<endl; value=val; right=r; left=l;}
+		Node(int val){value=val; right=NULL; left=NULL;}
+		Node(struct Node* l, struct Node* r, int val) {value=val; right=r; left=l;}
 	        
 		struct Node* getLeft() { return left;}
 		struct Node* getRight() {return right;}
@@ -30,53 +30,40 @@ namespace ariel
 		public:
 		
 		Tree() {
-			
-			cout<<"constructor tree"<<endl;
 			Troot=NULL;
 		}
 		
 		void insert(int key) {  
-			cout<<"insert "<<key<<endl;
 			Troot=insert(Troot, key);
-			cout<<"insert end"<<endl;
 		}
 		
 		void print() {
-			cout<<"print"<<endl;
 			if(Troot!=NULL) 
 				inorder_print(Troot);	
 			else 
 				cout<<"print: empty";
 			cout<<endl;
-			cout<<"print end"<<endl;
 		}
 		
 		void remove(int key) {
-			cout<<"remove "<<key<<endl;
 			Troot=remove(Troot,key);
-			cout<<"remove end"<<endl;
 		}
 
 		bool contains(int key) {
-			cout<<"contains "<<key<<endl;
 			if(Troot!=NULL){
 				if(Troot->getValue()==key)
 					return true;
 				else if(FindLeaf(Troot, key) !=NULL)
 					return true;
 			}
-			cout<<"contains end"<<endl;
 			return false;
 		}
 		
 		int size() {
-			cout<<"size"<<endl;
 			return size(Troot);
-			cout<<"size end"<<endl;
 		}
 		
 		int root() {
-			cout<<"root"<<endl;
 			if(Troot!=NULL) 
 				return Troot -> getValue(); 
 			throw std::invalid_argument ("root: tree is empty");
@@ -85,8 +72,7 @@ namespace ariel
 		int parent(int key) {
 			cout<<"parent"<<endl;
 			if(Troot->getValue()==key) 
-			{cout<<"parent end"<<endl;
-				return Troot->getValue(); }
+				throw std::invalid_argument("parent: root has no parent");
 			else {
 				struct Node* temp=Troot;
 				while (temp!=NULL){
@@ -105,10 +91,8 @@ namespace ariel
 		}
 		
 		int left(int key) {
-			cout<<"left"<<endl;
 			if(Troot->getValue()==key) 
-			{cout<<"left end"<<endl;
-				return Troot->getLeft()->getValue();}
+				return Troot->getLeft()->getValue();
 			else if(FindLeaf(Troot, key)==NULL || FindLeaf(Troot,key)->getLeft()==NULL) 
 				throw std::invalid_argument("left: no such value"); 
 			else 
@@ -116,10 +100,8 @@ namespace ariel
 		}
 		
 		int right(int key) {
-			cout<<"right"<<endl;
 			if (Troot->getValue()==key) 
-			{cout<<"right end"<<endl;
-				return Troot->getRight()->getValue();} 
+				return Troot->getRight()->getValue(); 
 			else if(FindLeaf(Troot, key)==NULL || FindLeaf(Troot, key) -> getRight()==NULL) 
 				throw std::invalid_argument("right: no such value"); 
 			else 
